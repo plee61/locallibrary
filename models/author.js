e85eb0af3737ca3ab955e3689e0603dee5776e29
hannah lee <plee61@hotmail.com>
2019-11-08 15:05:36 +1100

    First version of application moved into github

---
+++ models/author.js
@@ -0,0 +1,51 @@
+var mongoose = require('mongoose');
+var moment = require('moment');
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+  {
+    first_name: {type: String, required: true, max: 100},
+    family_name: {type: String, required: true, max: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+  }
+);
+
+// Virtual for author's full name
+AuthorSchema
+.virtual('name')
+.get(function () {
+  return this.family_name + ', ' + this.first_name;
+});
+
+// Virtual for author's URL
+AuthorSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/author/' + this._id;
+});
+
+// Virtual for author's lifespan
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
+  var age = moment(this.date_of_death).format('YYYY') - moment(this.date_of_birth).format('YYYY');
+  //return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+  return age;  
+});
+
+AuthorSchema
+.virtual('date_of_birth_formatted')
+.get(function () {
+  return this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '';
+});
+
+AuthorSchema
+.virtual('date_of_death_formatted')
+.get(function () {
+  return this.date_of_death ? moment(this.date_of_death).format('YYYY-MM-DD') : '';
+});
+
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
